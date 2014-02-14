1a7d19288308b62200129678c5035c631396a91f
Carina Brito <carinabs8@gmail.com>
2014-02-14 17:06:35 -0200

    Layout da applicação.

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,7 @@
+module ApplicationHelper
+
+	def dispatcher_route
+		controller_name = controller_path.gsub(/\//, "_")
+		"#{controller_name}##{action_name}"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Estudos::Application.config.secret_key_base = '95ef7b7e10faa87143d6adf7e0c505fe2c8b46fa36b85056cbab4b9b7696c5e17557db9c6bd45605965691d71320a44d94513be1ea0b024ceae957744913d838'
